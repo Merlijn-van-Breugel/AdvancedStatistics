2ef26c0b07b82ae810460216feca04dc11328741
Merlijn-van-Breugel <merlijnvanbreugel@gmail.com>
2017-03-18 14:18:23 +0100

    en go

--- DeterministicMCD.R
+++ DeterministicMCD.R
@@ -17,6 +17,12 @@
 install.packages("robustbase")
 library(robustbase)
 
+## Prepare data
+
+# We see quite a large number of cells with NA value
+# This is problematic for correlation and distance calculation, hence we omit these rows
+x <- na.omit(TopGear)
+x <- x[,9:15]
 
 ## Functions for initial estimators
 
@@ -26,7 +32,7 @@ library(robustbase)
 
 # correlation matrix based on the hyperbolic tangent
 corHT <- function(z) {
-    a <- apply(z,2, function(x) tanh(x),na.omit=TRUE)
+    a <- apply(z,2, function(x) tanh(x))
     return(cor(a))
 }
 
@@ -44,21 +50,38 @@ corNSR <- function(z) {
 
 # modified spatial sign covariance matrix
 covMSS <- function(z) {
-  # *enter your code here*
+    a <- apply(z,1, function(x) x/sqrt(sum(x^2)))
+    return((1/nrow(z))*a%*%t(a))
 }
 
 # covariance matrix based on first step of BACON
 covBACON1 <- function(z) {
-  # *enter your code here*
+    a <- apply(z,1,function(x) sqrt(sum(x^2)))
+    n <- ceiling(nrow(z)/2)
+    b <- z[order(a)[1:n],]
+    return(cov(b))
 }
 
 # raw OGK estimator of the covariance matrix with median and Qn
 # Hint: have a look at function covOGK() in package robustbase
 rawCovOGK <- function(z) {
-  # *enter your code here*
+    D <- diag(apply(z,2,function(x) Qn(x))) 
+    Z <- apply(z,1, function(x) solve(D)%*%x)
+    p <- ncol(z)
+    U <- diag(p)
+    for (i in 2:p) {
+        for (j in 1:(i - 1)) {
+            U[i, j] <- 1/4*(Qn(Z[,i]+Z[,j])^2 - Qn(Z[,i]-Z[,j])^2)}
+    }
+    E <- eigen(U, symmetric=TRUE)$vectors
+    V <- Z%*%E 
+    L <- diag(apply(V,2,function(x) Qn(x))^2)
+    SIG_rawogk <- D%*%E%*%L%*%t(E)%*%t(D)
+    return(SIG_rawogk)
 }
 
-
+# Put all covariance functions in a list
+cov.fun.list <- list(corHT, corSpearman, corNSR, covMSS, covBACON1, rawCovOGK)
 
 ## Main function for deterministic MCD algorithm
 
@@ -78,8 +101,8 @@ rawCovOGK <- function(z) {
 # best ......... indices of the observations in the best subset found by the
 #                raw estimator
 # any other output you want to return
-covDetMCD <- function(x, h, ...) {
-  
+covDetMCD <- function(x, h, cov.fun.list, eps, ...) {
+    
     # Standardize data
     medianWithoutNA<-function(x) {
         median(x[which(!is.na(x))])
@@ -95,14 +118,13 @@ covDetMCD <- function(x, h, ...) {
     # Get starting values from 6 robust covariance estimates of Z
     # Make list of functions, such that we can elegantly compute Sk
     # Step 1
-    covf.list <- list(corHT,corSpearman,corNSR,covMSS,covBACON1,rawCovOGK)
     S.list <- list()
     S.list$S1 <- corHT(Z)
     S.list$S2 <- corSpearman(Z)
     S.list$S3 <- corNSR(Z)
     S.list$S4 <- covMSS(Z)
     S.list$S5 <- covBACON1(Z)
-    S.list$S6 <- rawCovOGK(Z)
+    S.list$S6 <- rawCovOGK(Z)    
     
     eigen       <- lapply(S.list, eigen) 
     eigenvec    <- lapply(eigen, `[[`, 2)
@@ -119,8 +141,41 @@ covDetMCD <- function(x, h, ...) {
     
     # Get distance 
     D           <- lapply(seq(1,length(S.list)), function(x) mahalanobis(Z, mu[[x]],Sigma[[x]]))
-}
+   
+    # Get h smallest distance values
+    ind         <- lapply(D, function(x) sort(x)[1:h]) 
+    H.0         <- lapply(ind, function(x) Z[names(x),]) 
+    
+    
+    ## Write function to converge to smallest subset
+    loopTillConv <- function(H, eps){        
+        det.diff <- Inf
+        iter <- 1
+        while (det.diff > eps){
+            
+            # Compute mean and covariance of subset of Z (H)
+            H.mean  <- colMeans(H)
+            H.cov   <- cov(H)
+            # Get new mahalanobis distance of all data points Z, based on H.mean and H.cov
+            D       <- mahalanobis(Z, H.mean,H.cov)
+            # Get h points with smallest distance
+            ind     <- sort(D)[1:h]
+            H       <- Z[names(ind),] 
+            H.cov.new <- cov(H)
+            
+            det.diff <- det(H.cov) - det(H.cov.new)
+            iter    <- iter + 1
+        }
+        return(list(
+                    det.diff <- det.diff,
+                    det   <- det(H.cov.new),
+                    H.cov <- H.cov.new,
+                    iter  <- iter,
+                    H.mean <- colMeans(H)))
+    }
+    local.opt <- lapply(H.0, function(x) loopTillConv(x,eps))     
 
+}
 
 
 ## Function for regression based on the deterministic MCD
@@ -142,5 +197,5 @@ covDetMCD <- function(x, h, ...) {
 # any other output you want to return
 
 lmDetMDC <- function(x, y, h, ...) {
-  # *enter your code here*
+    # *enter your code here*
 }
